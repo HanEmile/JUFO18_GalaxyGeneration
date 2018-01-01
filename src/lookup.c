af0b25e9d5c28e6c7062602d0797b2ff983ea9d3
hanemile <emile.hansmaennel@gmail.com>
2018-01-01 18:30:13 +0100

    [milestone] print the lookuptable

--- src/lookup.c
+++ src/lookup.c
@@ -31,7 +31,7 @@ int main(int argc, char *argv[] ){
     int x = atoi(argv[1]);
 
     for(int i = 0; i < x; i++){
-      fprintf(fp, "%d, %f\n", i, phi(i));
+      printf("%d, %f\n", i, phi(i));
     }
 
     // for(int i = 0; i < 1e7; i = i + 1e4){
